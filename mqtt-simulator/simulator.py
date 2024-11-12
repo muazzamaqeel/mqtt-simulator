3c1184a81d2bbbbaa4d74d03bce38941d10150ba
muazzamaqeel <aqeemu01@thu.de>
2024-11-12 16:18:15 +0100

    FO

--- mqtt-simulator/simulator.py
+++ mqtt-simulator/simulator.py
@@ -1,4 +1,3 @@
-# simulator.py
 import json
 from topic import Topic
 from data_classes import BrokerSettings, ClientSettings
@@ -11,7 +10,8 @@ class Simulator:
             qos=2,
             time_interval=10
         )
-        self.topics = self.load_topics(settings_file)
+        self.settings_file = settings_file
+        self.topics = []
 
     def read_client_settings(self, settings_dict: dict, default: ClientSettings):
         return ClientSettings(
@@ -21,37 +21,35 @@ class Simulator:
             time_interval=settings_dict.get('TIME_INTERVAL', default.time_interval)
         )
 
-    def load_topics(self, settings_file):
+    def load_topics(self):
         topics = []
-        with open(settings_file) as json_file:
+        with open(self.settings_file) as json_file:
             config = json.load(json_file)
             broker_settings = BrokerSettings(
                 url=config.get('BROKER_URL', 'localhost'),
                 port=config.get('BROKER_PORT', 1883),
-                protocol=config.get('PROTOCOL_VERSION', 4) # mqtt.MQTTv311
+                protocol=config.get('PROTOCOL_VERSION', 4)
             )
             broker_client_settings = self.read_client_settings(config, default=self.default_client_settings)
             for topic in config['TOPICS']:
                 topic_data = topic['DATA']
                 topic_payload_root = topic.get('PAYLOAD_ROOT', {})
                 topic_client_settings = self.read_client_settings(topic, default=broker_client_settings)
-                if topic['TYPE'] == 'single':
-                    topic_url = topic['PREFIX']
-                    topics.append(Topic(broker_settings, topic_url, topic_data, topic_payload_root, topic_client_settings))
-                elif topic['TYPE'] == 'multiple':
-                    for id in range(topic['RANGE_START'], topic['RANGE_END']+1):
-                        topic_url = topic['PREFIX'] + '/' + str(id)
-                        topics.append(Topic(broker_settings, topic_url, topic_data, topic_payload_root, topic_client_settings))
-                elif topic['TYPE'] == 'list':
+                if topic['TYPE'] == 'list':
                     for item in topic['LIST']:
                         topic_url = topic['PREFIX'] + '/' + str(item)
                         topics.append(Topic(broker_settings, topic_url, topic_data, topic_payload_root, topic_client_settings))
-        return topics
+        self.topics = topics
 
     def run(self):
+        self.load_topics()  # Load topics to initialize threads
         while True:
+            # Start each topic thread
             for topic in self.topics:
-                print(f'Starting: {topic.topic_url} ...')
-                topic.start()
+                if not topic.is_alive():  # Check if thread is not running
+                    print(f'Starting: {topic.topic_url} ...')
+                    topic.start()
+
+            # Join threads to wait for their completion
             for topic in self.topics:
                 topic.join()
